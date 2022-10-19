88c2ad7de331155bb84e244d9ef658561e8ecde3
Babajide Oke <josua.oke@solera.com>
2022-10-19 10:02:33 +0100

    Main obj done

--- src/main/java/com/solera/training/demo/Exc.java
+++ src/main/java/com/solera/training/demo/Exc.java
@@ -1,9 +1,39 @@
 package com.solera.training.demo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Exc {
-	
+
 	public static void main(String[] args) {
-		System.out.println("Hello from excercise");
+		
+		Scanner myObj = new Scanner(System.in);
+		Scanner myObj2 = new Scanner(System.in);
+		int exit = 0;
+		ArrayList<String> notes = new ArrayList<>();
+		String note = " ";
+		
+		
+		
+		// 1 to store note, 2 to exit
+		while (exit != 2) {
+			System.out.println("1 to store note, 2 to exit, 3 to view notes");
+			exit = myObj.nextInt();
+			
+			if(exit==1) {
+				System.out.println("Type the note you would like to save: ");
+				note = myObj2.nextLine();
+				
+				notes.add(note);
+			}
+			else if(exit == 3) {
+				System.out.println(notes);
+			}
+			else;
+			
+		}
+
+		System.out.println("Adios");
 	}
 
 }
