a5e005ad5c599fa2d3961f2439f56c057b4e3614
Babajide Oke <josua.oke@solera.com>
2022-10-19 09:27:00 +0100

    First commit

---
+++ src/main/java/com/solera/training/demo/BootcampExcApplication.java
@@ -0,0 +1,13 @@
+package com.solera.training.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampExcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampExcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solera/training/demo/BootcampExcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solera.training.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampExcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
