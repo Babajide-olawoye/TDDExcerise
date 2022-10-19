e70498f89ce473dc5f7087c10e4f2cb84af0c256
Kirylo <dainius.kirylo@solera.com>
2022-10-19 13:32:20 +0100

    New test cases and features

--- src/main/java/com/solera/training/demo/Exc.java
+++ src/main/java/com/solera/training/demo/Exc.java
@@ -4,37 +4,56 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Exc {
+	
 	ArrayList<String> notes = new ArrayList<>();
+	
 	
 	public ArrayList<String> getNotes() {
 		return notes;
 	}
-
-
-	public void saveNotes(String note) {
-			
-		notes.add(note);
-		
+	
+	//Update notes
+	public void updateNotes(int index, String newNote) {
+		notes.set(index, newNote);
 	}
 	
+	//Add notes
+	public void saveNotes(String note) {
+		notes.add(note);
+	}
 	
-	
+	//View notes
 	public void viewNotes() {
 		System.out.println(notes);
+	}
+	
+	//Delete notes
+	public void deleteNotes() {
+		notes.clear();
+	}
+	
+	//Delete last note
+	public void deleteLastNote() {
+		int index = notes.size() - 1;   
+		notes.remove(index);
 	}
 
 	public static void main(String[] args) {
 		
 		Exc exc = new Exc();
-		Scanner myObj2 = new Scanner(System.in);
 		Scanner myObj = new Scanner(System.in);
+		Scanner myObj2 = new Scanner(System.in);
+		Scanner myObj3 = new Scanner(System.in);
+		Scanner myObj4 = new Scanner(System.in);
 		int exit = 0;
+		int index = 0;
 		String note = " ";
+		String newNote;
 		
 			
 		// 1 to store note, 2 to exit
 		while (exit != 2) {
-			System.out.println("1 to store note, 2 to exit, 3 to view notes");
+			System.out.println("1 to store note, 2 to exit, 3 to view notes, 4 to clear the list, 5 to clear the last note or 6 to update an entry");
 			exit = myObj.nextInt();
 
 			if(exit == 1) {
@@ -42,10 +61,24 @@ public class Exc {
 				note = myObj2.nextLine();
 				exc.saveNotes(note);
 			}
-			else if(exit == 3) {
+			if(exit == 3) {
 				exc.viewNotes();
 			}
-			
+			if(exit == 4) {
+				exc.deleteNotes();
+			}
+			if(exit == 5) {
+				exc.deleteLastNote();
+			}
+			else if(exit == 6){
+				System.out.println("Please enter the index you would like to edit");
+				index = myObj3.nextInt();
+				
+				System.out.println("Please enter the updated note");
+				newNote = myObj4.nextLine();
+				exc.updateNotes(index, newNote);
+				
+			}
 		}
 
 	}
--- src/test/java/com/solera/training/test/ExcTest.java
+++ src/test/java/com/solera/training/test/ExcTest.java
@@ -33,5 +33,40 @@ class ExcTest {
 		assertNotEquals(testVal, exc.getNotes().get(1));
 		
 	}
+	
+	@Test
+	void checkIfEmpty() {
+		Exc exc = new Exc();
+		
+		exc.saveNotes("Testing on User input");
+		exc.deleteLastNote();
+		
+		assertEquals(exc.getNotes().isEmpty(), true);
+		
+	}
+	
+	@Test
+	void checkIfNotEmpty() {
+		Exc exc = new Exc();
+		
+		exc.saveNotes("Testing on User input");
+		exc.saveNotes("Testing on Users second input");
+		exc.deleteLastNote();
+		
+		assertNotEquals(exc.getNotes().isEmpty(), true);
+	}
+	
+	@Test
+	void checkIfClear() {
+		Exc exc = new Exc();
+		
+		exc.saveNotes("Note 1");
+		exc.saveNotes("Note 2");
+		exc.saveNotes("Note 3");
+		exc.deleteNotes();
+		
+		assertEquals(exc.getNotes().isEmpty(), true);
+		
+	}
 
 }
